fa922016813d03a3f8d650ac19f77f3f3ba0d139
Abhishek Kumar Singh <abhisheksingh1911a5@gmail.com>
2022-05-18 16:26:49 +0530

    picker

--- src/Components/EmployeeCreate.js
+++ src/Components/EmployeeCreate.js
@@ -2,6 +2,8 @@ import React,{Component} from 'react';
 import { Card, CardSection, Input, Button, Spinner } from './common';
 import {connect} from 'react-redux';
 import {employeeUpdate} from '../actions';
+import {Text} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 class EmployeeCreate extends Component{
     render(){
@@ -23,7 +25,26 @@ class EmployeeCreate extends Component{
                         onChangeText={text => this.props.employeeUpdate({prop: 'phone',value: text})}
                     />
                 </CardSection>
-                <CardSection>
+                <CardSection
+                    // eslint-disable-next-line react-native/no-inline-styles
+                    style={{
+                        flexDirection: 'column',
+                        height:100,
+                    }}>
+                    <Text style={styles.picker}>{this.props.shift}</Text>
+                <Picker
+                    // eslint-disable-next-line react-native/no-inline-styles
+                    style={{flex:1,marginLeft:10}}
+                    selectedValue={this.props.shift}
+                    onValueChange={day => this.props.employeeUpdate({prop: 'shift', value: day})}
+                >
+                    <Picker.Item label="Monday" value="Monday"/>
+                    <Picker.Item label="Tuesday" value="Tuesday"/>
+                    <Picker.Item label="Wednesday" value="Wednesday"/>
+                    <Picker.Item label="Thursday" value="Thursday"/>
+                    <Picker.Item label="Friday" value="Friday"/>
+                    <Picker.Item label="Saturday" value="Saturday"/>
+                </Picker>
                 </CardSection>
                 <CardSection>
                 <Button>
@@ -34,6 +55,13 @@ class EmployeeCreate extends Component{
         );
     }
 }
+
+const styles = {
+    picker:{
+        fontSize: 18,
+        paddingLeft:20,
+    },
+};
 
 const mapStateToProps = (state) => {
     const {name,phone, shift} = state.employeeform;
